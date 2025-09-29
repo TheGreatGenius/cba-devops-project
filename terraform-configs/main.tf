8bf45154a62ef86b5fb052f2f65797a50638398e
thegreatgenius <davidjinanwa24@gmail.com>
2025-09-29 16:31:32 +0100

    adding more configs.

--- terraform-configs/main.tf
+++ terraform-configs/main.tf
@@ -1,21 +1,29 @@
-terraform{
-    required_providers {
-        minikube = {
-            source = "scott-the-programmer/minikube"
-            version = "0.4.2"
-        }
+terraform {
+  required_providers {
+    minikube = {
+      source  = "scott-the-programmer/minikube"
+      version = "0.4.2"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0" # latest stable you mentioned earlier
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.1.2"  # match your tutorial or upgrade if needed
+    }
+  }
 }
 
 provider "minikube" {
-    kubernetes_version = "v1.30.0"
+  kubernetes_version = "v1.30.0"
 }
 
 resource "minikube_cluster" "minikube_docker" {
-    driver = "docker"
-    cluster_name = "cba-devops-project"
-    addons = [
-        "default-storageclass",
-        "storage-provisioner"
-    ]
+  driver       = "docker"
+  cluster_name = "cba-devops-project"
+  addons = [
+    "default-storageclass",
+    "storage-provisioner"
+  ]
 }
--- terraform-configs/provider.tf
+++ terraform-configs/provider.tf
@@ -1,6 +1,23 @@
-provider "kubernetes"{
-    host = minikube_cluster.minikube_docker.host
-    client_certificate = minikube_cluster.minikube_docker.client_certificate
-    client_key = minikube_cluster.minikube_docker.client_key
-    cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
+provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
